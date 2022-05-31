f15f967cfbf4cea8bdc7fd51ab528089e5a97a6c
Giao Ngo <giao.ngo@metropolia.fi>
2022-05-31 13:58:34 +0300

    Initialize WordDatabase, WordRepository, WordViewModelFactory. Initiate livedata observing and update ui accordingly.

--- app/src/main/java/fi/giao/wordguessinggame/game/DoublePlayFragment.kt
+++ app/src/main/java/fi/giao/wordguessinggame/game/DoublePlayFragment.kt
@@ -1,31 +1,53 @@
 package fi.giao.wordguessinggame.game
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import fi.giao.wordguessinggame.MainActivity
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import fi.giao.wordguessinggame.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DoublePlayFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+import fi.giao.wordguessinggame.database.WordDatabase
+import fi.giao.wordguessinggame.database.WordRepository
+import fi.giao.wordguessinggame.databinding.FragmentDoublePlayBinding
+import fi.giao.wordguessinggame.ui.WordViewModel
+import fi.giao.wordguessinggame.ui.WordViewModelFactory
 class DoublePlayFragment : Fragment() {
+    private lateinit var binding: FragmentDoublePlayBinding
+    private lateinit var wordViewModel: WordViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_double_play, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_double_play,container,false)
 
+        val wordDatabase = WordDatabase(requireActivity())
+        val wordRepository  = WordRepository.getInstance(wordDatabase.getWordDao())
+        val wordViewModelFactory = WordViewModelFactory(wordRepository)
+        wordViewModel = ViewModelProvider(this,wordViewModelFactory)[WordViewModel::class.java]
+        wordViewModel.word.observe(viewLifecycleOwner, Observer {
+            binding.word.text  = it
+        })
+        wordViewModel.score.observe(viewLifecycleOwner, Observer {
+            binding.scoreDouble.text = requireActivity().getString(R.string.score,it)
+        })
+        onCorrectButtonClicked()
+        onSkipButtonClick()
+
+        return binding.root
+    }
+    private fun  onCorrectButtonClicked() {
+        binding.correctButton.setOnClickListener{
+            wordViewModel.onCorrect()
+        }
+    }
+    private fun onSkipButtonClick() {
+        binding.skipButton.setOnClickListener{
+            wordViewModel.onSkip()
+        }
     }
 }
--- app/src/main/java/fi/giao/wordguessinggame/ui/MainActivity.kt
+++ app/src/main/java/fi/giao/wordguessinggame/ui/MainActivity.kt
@@ -1,11 +1,12 @@
-package fi.giao.wordguessinggame
+package fi.giao.wordguessinggame.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import androidx.navigation.Navigation
+import android.util.Log
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import fi.giao.wordguessinggame.R
+import fi.giao.wordguessinggame.database.WordDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
